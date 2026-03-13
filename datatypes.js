4bbfbc4ee97ab323011f0ed9dbea507460b34d7e
govardhanreddykovvuri878-boop <govardhanreddykovvuri878@gmail.com>
2026-03-13 14:52:23 +0530

    Add files via upload

---
+++ datatypes.js
@@ -0,0 +1,243 @@
+// Data Types
+// Task 1
+let name = "Govardhan";
+console.log(name);
+
+// Task 2
+console.log(typeof("JavaScript"));
+
+// Task 3
+let age = 22;
+console.log(typeof(age));
+
+// Task 4
+let isStudent = true;
+console.log(typeof isStudent);
+
+// Task 5
+var salary;
+console.log(salary);
+
+// Array Tasks
+// Task 6
+let fruits =["Apple","Banana","Orange","Grapes","Kiwi"];
+console.log(fruits);
+
+// Task 7
+console.log(fruits[0]);
+
+// Task 8
+console.log(fruits[2]);
+
+// Task 9
+console.log(fruits[fruits.length-1]);
+
+// Task 10
+let numbers = [10,20,30,40,50];
+console.log(numbers[1]); // second element
+console.log(numbers[3]); // Fourth element
+
+// Object Tasks
+// Task 11
+let person = {
+    name1 : "Govardhan",
+    age : 22,
+    city : "Kadapa"
+};
+console.log(person);
+
+// Task 12
+console.log(person.name1);
+
+// Task 13
+person.skills = ["HTML","CSS","JS"];
+console.log(person.skills[1]);
+
+// Task 14
+let carDetails = {
+    brand : "kia",
+    model : 2024,
+    price : 2000000
+};
+console.log(carDetails.brand);
+
+// Task 15
+let fruitCategory = {
+    red : ["apple","cherry"],
+    yellow : ["banana","mango"],
+    green : ["kiwi","grapes"]
+};
+console.log(fruitCategory.yellow[0]);
+
+// Arthimetic Operators
+// Task 16
+let a = 8;
+let b = 4;
+console.log(a + b);
+
+// Task 17
+console.log(a - b);
+
+// Task 18
+console.log(a * b);
+
+// Task 19
+console.log(a / b);
+
+// Task 20
+console.log(a % b);
+ 
+// Exponential Operator
+// Task 21
+let a1 = 3;
+let b2 = 2;
+console.log(a1 ** b2);
+
+// Task 22
+let a2 = 2;
+let b3 = 4;
+console.log(a2 ** b3);
+
+// Increment/Decrement
+// Task 23
+let num = 5;
+num ++;
+console.log(num);
+
+// Task 24
+let num1 = 10;
+num1 --;
+console.log(num1);
+
+// Task 25
+let x = 6;
+let y = x++;
+console.log(x);
+console.log(y);
+
+// Task 26
+let a4 = 4;
+let b4 = ++ a4;
+console.log(a4);
+console.log(b4);
+
+// Assinment Operators
+// Task 27
+let value = 10;
+value += 5;
+console.log(value);
+
+// Task 28
+let value1 = 20;
+value1 -= 3;
+console.log(value1);
+
+// Task 29
+let value3 = 6;
+value3 *= 2;
+console.log(value3);
+
+// Task 30
+let value4 = 20;
+value4 %= 3;
+console.log(value4);
+
+// Comparison Operators
+// Task 31
+10 == "10"
+console.log(10 == "10");
+
+// Task 32
+10 ==="10"
+console.log(10 ==="10");
+
+// Task 33
+15 > 20
+console.log(15 > 20);
+
+// Task 34
+5 <= 5
+console.log(5 <= 5);
+
+// Logical Operators
+// Task 35
+10>5&&5>2
+console.log(10>5&&5>2);
+// Task 36
+10<5||8>2
+console.log(10<5||8>2);
+
+// Task 37
+!(5 === "5")
+console.log(!(5 === "5"));
+
+//Ternary Operator
+// Task 38
+let age1 = 20;
+console.log(age1 >= 18 ? "Adult" : "Minor");
+
+// Task 39
+let password = 1;
+console.log(password = 1  ? "Login Success" : "Wrong Password");
+
+// String Operations
+let firstName = "Spider";
+let lastName = "Man";
+console.log(firstName + " " + lastName);
+console.log(`${firstName} ${lastName}`);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
